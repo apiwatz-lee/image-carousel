92063cdc20ced3ad251fca8b3a98b3c5c9c2bda9
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-11-01 16:38:16 +0700

    image:user can slide picture

---
+++ src/data/ImageData.jsx
@@ -0,0 +1,18 @@
+const ImageData = [
+  {
+    title: "Cat",
+    image: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+  },
+  {
+    title: "Buddha",
+    image:
+      "https://cdn.pixabay.com/photo/2020/06/30/22/52/monk-5357843_1280.jpg",
+  },
+  {
+    title: "Thailand",
+    image:
+      "https://cdn.pixabay.com/photo/2018/06/11/15/15/asia-3468649_1280.jpg",
+  },
+];
+
+export default ImageData;
